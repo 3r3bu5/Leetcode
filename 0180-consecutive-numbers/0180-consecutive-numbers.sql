c27d9c73e50359c5bdc0162c3edceea7741173cd
Ashraf Gehad Yehia <33455548+3r3bu5@users.noreply.github.com>
2023-01-15 15:52:47 +0200

    Time: 1080 ms (12.09%), Space: 0B (100.00%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT 
+        id,
+        LAG(num) OVER (ORDER BY id) AS PrevNum,
+        num,
+        LEAD(num) OVER (ORDER BY id) AS NextNum
+    FROM Logs) l
+WHERE num = PrevNum AND num = NextNum
